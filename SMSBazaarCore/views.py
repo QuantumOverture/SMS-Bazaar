fb43c47daa7de996383bf7fec8262e751b1a9cae
Alexandros Ali Khan <axk1168@miami.edu>
2020-09-26 18:03:35 +0300

    fixed views.py

--- SMSBazaarCore/views.py
+++ SMSBazaarCore/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from twilio.rest import client
 
 
 
 def index(request):
     if request.method == 'GET':
-        return HTTpResponse("Go away.")
+        return HttpResponse("Go away.")
     elif request.method == 'POST':
-        return HttpResponse(str(request.body))
+        account_sid = 'AC3dfc80e02f7e984afb857ba4900a0fcd'
+        auth_token = 'e36aab7c7237ee2c0e149f2de034c944'
+        client = Client(account_sid, auth_token)
+        message = client.messages \
+            .create(
+                body=str(request.body)pip,
+                from_='+18772579727',
+                to='+14156906522'
+            )                                                                                                                                                                                                                    from_='+18559651822',
+                                                                                                                                                                                                                                to='+14156906522'
+                                                                                                                                                                                                                                )
+
+        print(message.sid)
