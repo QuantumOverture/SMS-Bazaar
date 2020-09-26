5833f71d73d90935b37841c11998753f60c91822
Alexandros Ali Khan <axk1168@miami.edu>
2020-09-26 18:43:59 +0300

    fixed views.py again 5

--- SMSBazaarCore/views.py
+++ SMSBazaarCore/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from twilio.rest import Client
 from twilio.twiml.messaging_response import Message, MessagingResponse
 
 
-
+@csrf_exempt
 def index(request):
     if request.method == 'GET':
         return HttpResponse("Go away.")
