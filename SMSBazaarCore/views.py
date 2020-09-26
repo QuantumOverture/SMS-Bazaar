5b286740fc0199a58582f6f55a3ed0c1d6ef85e4
Alexandros Ali Khan <axk1168@miami.edu>
2020-09-26 18:24:16 +0300

    fixed views.py again 5

--- SMSBazaarCore/views.py
+++ SMSBazaarCore/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
 
 
 
@@ -8,7 +9,7 @@ def index(request):
     if request.method == 'GET':
         return HttpResponse("Go away.")
     elif request.method == 'POST':
-        account_sid = 'AC3dfc80e02f7e984afb857ba4900a0fcd'
+        """account_sid = 'AC3dfc80e02f7e984afb857ba4900a0fcd'
         auth_token = 'e36aab7c7237ee2c0e149f2de034c944'
         client = Client(account_sid, auth_token)
         message = client.messages \
@@ -17,4 +18,12 @@ def index(request):
                 from_='+18772579727',
                 to='+14156906522'
             )
-        print(message.sid)
+        print(message.sid)"""
+        """Respond to incoming calls with a simple text message."""
+        # Start our TwiML response
+        resp = MessagingResponse()
+
+        # Add a message
+        resp.message("The Robots are coming! Head for the hills!")
+
+        return str(resp)
